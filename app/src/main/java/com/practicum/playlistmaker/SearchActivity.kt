8f752f89a32d4fefcb196b674a8c9b235c370518
Ildar <nuruda@yandex.ru>
2024-05-03 09:45:34 +0300

    New feature added

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -2,10 +2,64 @@ package com.practicum.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageButton
 
 class SearchActivity : AppCompatActivity() {
+
+    private var text = ""
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("text", text)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        text = savedInstanceState.getString("text", "")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val buttonBack = findViewById<ImageButton>(R.id.buttonBackToMenu)
+        val searchEditText = findViewById<EditText>(R.id.inputEditText)
+        val clearButton = findViewById<ImageButton>(R.id.clearButton)
+
+
+
+        buttonBack.setOnClickListener {
+            finish()
+        }
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (s.isNullOrEmpty()) {
+                    clearButton.visibility = View.GONE
+                } else {
+                    clearButton.visibility = View.VISIBLE
+                    text = s.toString()
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
+
+        if (savedInstanceState != null) {
+            searchEditText.setText(text)
+        }
+
+        clearButton.setOnClickListener {
+            searchEditText.setText("")
+            clearButton.visibility = View.GONE
+        }
+
+
     }
 }
